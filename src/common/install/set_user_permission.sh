832fe2cdaceec2087aab382e8d99d049618d06fa
nhat doan <nhatdoanexpo@gmail.com>
2023-03-30 21:29:48 +0700

    up

--- src/common/install/set_user_permission.sh
+++ src/common/install/set_user_permission.sh
@@ -10,5 +10,7 @@ do
     echo "fix permissions for: $var"
     find "$var"/ -name '*.sh' -exec chmod $verbose a+x {} +
     find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
+    find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
     chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
 done
+chmod -R a+rwx /opt

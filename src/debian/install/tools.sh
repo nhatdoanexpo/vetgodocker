83b1f7c69bd3b19e585349a676b74e121fd01f9c
nhatdoanexpo <93253929+nhatdoanexpo@users.noreply.github.com>
2023-03-30 19:21:25 +0700

    Update tools.sh

--- src/debian/install/tools.sh
+++ src/debian/install/tools.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update
-apt-get install -y vim wget net-tools locales bzip2 procps unzip zip curl gnupg software-properties-common  \
+apt-get install -y vim wget net-tools locales bzip2 procps unzip zip curl git gnupg software-properties-common  \
 python3-numpy
 #used for websockify/novnc
 apt-get install -y sudo && echo 'root:vetgo' | chpasswd

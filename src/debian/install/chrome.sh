a967d6bd8e81a7bd827407ea8d02c1d7df38b655
duynh <nhat.doan.expo@gmail.com>
2024-08-15 21:20:39 +0700

    up

--- src/debian/install/chrome.sh
+++ src/debian/install/chrome.sh
@@ -6,3 +6,6 @@ echo "Install Chromium Browser"
 apt-get install -y chromium
 ln -sfn /usr/bin/chromium /usr/bin/chromium-browser
 apt-get clean -y
+rm -rf ~/.config/chromium
+
+echo "Chromium installation complete and configuration cleaned up."

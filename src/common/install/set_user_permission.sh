ce76db42ada43299805ca55d86c5a22c6586d259
duynh <nhat.doan.expo@gmail.com>
2024-12-15 23:38:20 +0700

    up node

--- src/common/install/set_user_permission.sh
+++ src/common/install/set_user_permission.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-if [[ -n $DEBUG ]]; then
-    verbose="-v"
+
+# Set locale if necessary
+if [ ! -d "/opt" ]; then
+    echo "/opt directory not found, creating it."
+    mkdir -p /opt
 fi
 
-for var in "$@"
-do
+# Fix permissions for provided directories
+for var in "$@"; do
     echo "fix permissions for: $var"
-    find "$var"/ -name '*.sh' -exec chmod $verbose a+x {} +
-    find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
-    chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
+    find "$var"/ -name '*.sh' -exec chmod a+x {} +
+    find "$var"/ -name '*.desktop' -exec chmod a+x {} +
+    chgrp -R 0 "$var" && chmod -R a+rw "$var" && find "$var" -type d -exec chmod a+x {} +
 done
+
+# Handle /opt permissions
 if [ -d "/opt" ]; then
     chgrp -R 1000 /opt/* && chmod -R a+rwx /opt/*
 else
